49d9ba4623f790bd191f28a6ff1a9a773ab9228a
Cuong3cdz <lop8a82003@gmail.com>
2025-06-12 21:43:10 +0700

    update Home
    setting game

---
+++ Assets/PhamCuong/Scrip/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // tồn tại qua các scene
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    // Gọi play sfx
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/PhamCuong/Scrip/AudioSettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSettingsManager : MonoBehaviour
+{
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private void Start()
+    {
+        // Load saved values or default to 1.0f
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        // Set listeners
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        AudioListener.volume = value; // toàn bộ âm thanh
+        PlayerPrefs.SetFloat("MasterVolume", value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        // Giả sử có AudioMixer hoặc nhạc nền riêng
+        AudioManager.Instance.SetMusicVolume(value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        AudioManager.Instance.SetSFXVolume(value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+}
