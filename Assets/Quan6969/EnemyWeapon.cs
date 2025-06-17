e54fd37b028444592adf93a1a6318b62d73d0767
kob2k560 <kob200459@gmail.com>
2025-06-17 20:28:19 +0700

    thanh mau, nang luong

--- Assets/Quan6969/EnemyWeapon.cs
+++ Assets/Quan6969/EnemyWeapon.cs
@@ -1,15 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 public class EnemyWeapon : MonoBehaviour
 {
-    public float baseDamage = 8f;
-    public float bonusDamage = 3f;
-    public float critRate = 0.1f;
+    public float baseDamage = 10f;
+    public float bonusDamage = 5f;
+    public float critRate = 0.2f;
 
     private HashSet<GameObject> damagedPlayers = new HashSet<GameObject>();
 
-    public void ResetDamage()
+    // Gọi cái này mỗi khi enemy bắt đầu đòn đánh mới
+    public void AttackStart()
+    {
+        ResetDamage();
+    }
+
+    private void ResetDamage()
     {
         damagedPlayers.Clear();
     }
@@ -24,7 +30,7 @@ public class EnemyWeapon : MonoBehaviour
                 bool isCrit = Random.value < critRate;
                 DamageInfo damage = new DamageInfo(baseDamage, bonusDamage, isCrit);
                 player.TakeDamage(damage);
-                damagedPlayers.Add(other.gameObject);
+                damagedPlayers.Add(other.gameObject); // nhớ đã đánh rồi
             }
         }
     }
--- Assets/Quan6969/PlayerHealth.cs
+++ Assets/Quan6969/PlayerHealth.cs
@@ -1,20 +1,28 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHP = 100f;
     private float currentHP;
 
+    public Image healthBarFill; // Kéo Image vào đây
+
     void Start()
     {
         currentHP = maxHP;
+        UpdateHealthBar();
     }
 
     public void TakeDamage(DamageInfo damage)
     {
         float amount = damage.GetTotalDamage();
         currentHP -= amount;
-        Debug.Log($"Người chơi nhận {amount} dame. HP còn lại: {currentHP}");
+        currentHP = Mathf.Clamp(currentHP, 0, maxHP);
+
+        Debug.Log($"Player mất {amount} máu. HP còn: {currentHP}");
+        UpdateHealthBar();
 
         if (currentHP <= 0)
         {
@@ -22,9 +30,26 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void Heal(float amount)
+    {
+        currentHP += amount;
+        currentHP = Mathf.Min(currentHP, maxHP);
+
+        Debug.Log($"Player hồi {amount} máu. HP: {currentHP}");
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = currentHP / maxHP;
+        }
+    }
+
     void Die()
     {
-        Debug.Log("Người chơi đã chết.");
-        // Add logic chết ở đây
+        Debug.Log("Player đã chết.");
     }
 }
+
