7d650416b4d346a1f1b4c401c75da4134e63063b
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-06-30 15:56:09 +0700

    Add melee weapon
    Thêm 9 vũ khí cận chiến mới, thêm phương thức tấn công cho vũ khí cung , điều chỉnh phương thức tấn công một vài vũ khí cung.

--- Assets/NguyenDat/Script/Weapon/Bow/AutoDestroy.cs
+++ Assets/NguyenDat/Script/Weapon/Bow/AutoDestroy.cs
@@ -5,14 +5,25 @@ public class AutoDestroy : MonoBehaviour
     [Header("Projectile Identity")]
     public bool IsPlayer = false;
     public bool IsEnemy = false;
+
+    [Header("Projectile Behavior")]
+    public bool canPierce = false;
     // Tự hủy khi chạm vào Wall, Enemy hoặc Boss
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Wall"))
         {
             Destroy(gameObject);
+            return;
         }
-        else if (IsPlayer && (collision.CompareTag("Enemy") || collision.CompareTag("Boss")))
+
+        if (canPierce)
+        {
+            // Đạn xuyên mục tiêu thì không hủy khi chạm bất cứ gì ngoài tường
+            return;
+        }
+
+        if (IsPlayer && (collision.CompareTag("Enemy") || collision.CompareTag("Boss")))
         {
             Destroy(gameObject);
         }
