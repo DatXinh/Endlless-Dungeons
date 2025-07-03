ed489239a0f4976ca058a7156a6080cd5582f65a
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-07-03 21:10:27 +0700

    cập nhật scense đánh boss

--- Assets/NguyenDat/Script/DungeonGenerator/RoomData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
-public class RoomData : MonoBehaviour
-{
-    public Vector2Int gridPosition;
-    public Vector2Int roomSize;
-    public int nearRoom;
-}
