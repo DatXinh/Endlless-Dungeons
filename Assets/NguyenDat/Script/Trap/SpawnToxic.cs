01546ed31d711c4fc315959a3e12148af09a860e
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-07-04 10:07:28 +0700

    Điều chỉnh script bắn đạn ; thêm deco , trap ; sinh thêm trap trong hầm ngục

---
+++ Assets/NguyenDat/Script/Trap/SpawnToxic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SpawnToxic : MonoBehaviour
+{
+    public GameObject spawnPrefab;
+    private EnemyHealth enemyHealth;
+    private bool hasSpawned = false;
+
+    private void Awake()
+    {
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    private void Update()
+    {
+        if (!hasSpawned && enemyHealth.currentHP <= 0)
+        {
+            hasSpawned = true;
+
+            GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation);
+
+            // Gán tên giống object cũ nếu cần debug
+            clone.name = spawnPrefab.name;
+
+            // Sau khi spawn prefab, tự xóa object (giống hành vi gốc)
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/NguyenDat/Script/Weapon/Bow/AutoDestroyProjectile.cs
+++ Assets/NguyenDat/Script/Weapon/Bow/AutoDestroyProjectile.cs
@@ -12,13 +12,15 @@ public class AutoDestroyProjectile : MonoBehaviour
     [Header("LifeTime")]
     public float lifeTime = 1f;
 
+    private SpawnAtDestroy spawnAtDestroy;
+
     private void Start()
     {
         Destroy(gameObject,lifeTime);
     }
 
     // Tự hủy khi chạm vào Wall, Enemy hoặc Boss
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Wall"))
         {
@@ -38,6 +40,21 @@ public class AutoDestroyProjectile : MonoBehaviour
         else if (IsEnemy && collision.CompareTag("Player"))
         {
             Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (spawnAtDestroy == null)
+        {
+            spawnAtDestroy = GetComponent<SpawnAtDestroy>();
+            if (spawnAtDestroy == null)
+            {
+                return;
+            }
+            if (spawnAtDestroy != null)
+            {
+                spawnAtDestroy.TriggerDestroy();
+            }
         }
     }
 }
