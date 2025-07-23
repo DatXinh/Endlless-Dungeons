d0cf51e3c6beff596a56e114b38179a1a50dcac1
Cuong3cdz <126306100+Cuong3cdz@users.noreply.github.com>
2025-07-24 00:10:26 +0700

    update map

---
+++ Assets/PhamCuong/Scrip/CamFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 newPos = target.position;
+            newPos.z = transform.position.z; // giữ nguyên chiều sâu camera
+            transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/PhamCuong/Scrip/MiniMapPresist.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MiniMapPresist : MonoBehaviour
+{
+    private static MiniMapPresist instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject); // Tránh tạo trùng khi load lại scene
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded; // đăng ký sự kiện
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string currentScene = scene.name;
+
+        // Danh sách các scene KHÔNG cần minimap
+        if (currentScene == "Star Game" || currentScene == "Home")
+        {
+            SetMinimapVisible(false);
+        }
+        else
+        {
+            SetMinimapVisible(true);
+        }
+    }
+
+    void SetMinimapVisible(bool visible)
+    {
+        // Ẩn hoặc hiện camera minimap
+        Transform minimapCamera = transform.Find("MinimapCamera");
+        if (minimapCamera) minimapCamera.gameObject.SetActive(visible);
+
+        // Ẩn hoặc hiện RawImage
+        
+        Transform rawImage = transform.Find("MinimapDisplay");
+        if (rawImage) rawImage.gameObject.SetActive(visible);
+        //border
+        Transform border = transform.Find("Border");
+        if (border) border.gameObject.SetActive(visible);
+    }
+}
