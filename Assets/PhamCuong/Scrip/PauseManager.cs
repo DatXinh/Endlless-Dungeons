fa3be1acb0343864c4c70677de08c250c58ea8ae
Cuong3cdz <126306100+Cuong3cdz@users.noreply.github.com>
2025-07-26 22:12:45 +0700

    update button

---
+++ Assets/PhamCuong/Scrip/PauseManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu; // Gán canvas vào đây
+
+    private bool isPaused = false;
+
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+}
