aed69deb2d5e1a0889343a8db3120d3beb47de18
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-08-01 16:03:27 +0700

    fix lỗi vũ khí không gây sát thương và những lỗi nhỏ.

--- Assets/NguyenDat/Script/TestPlayer/PlayerDontDestroyOnLoad.cs
+++ Assets/NguyenDat/Script/TestPlayer/PlayerDontDestroyOnLoad.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerDontDestroyOnLoad : MonoBehaviour
 {
     private static PlayerDontDestroyOnLoad instance;
     public GameObject child;
+
+    [Header("UI")]
+    public TextMeshProUGUI sceneNameText;
+    public TextMeshProUGUI playTimeText;
+
+    public Camera mainCamera;
+
+    private float playTime = 0f;
 
     private void Awake()
     {
@@ -18,22 +28,62 @@ public class PlayerDontDestroyOnLoad : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        SetPhysicalCameraByScene(SceneManager.GetActiveScene());
+    }
+
+    private void Update()
+    {
+        playTime += Time.deltaTime;
+
+        if (playTimeText != null)
+        {
+            int minutes = Mathf.FloorToInt(playTime / 60f);
+            int seconds = Mathf.FloorToInt(playTime % 60f);
+            playTimeText.text = $"Thời gian chơi: {minutes:D2}:{seconds:D2}";
+        }
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Đặt lại vị trí mỗi khi load scene mới
-        transform.position = Vector3.zero; // (0,0,0)
-        // Nếu có child, đặt lại vị trí của nó
+        // Đặt lại vị trí
+        transform.position = Vector3.zero;
         if (child != null)
         {
-            child.transform.position = Vector3.zero; // (0,0,0)
+            child.transform.position = Vector3.zero;
+        }
+
+        // Cập nhật tên màn chơi
+        if (sceneNameText != null)
+        {
+            sceneNameText.text = $"Màn chơi: {scene.name}";
         }
     }
+    public void ResetPlayTime()
+    {
+        playTime = 0f;
 
+        if (playTimeText != null)
+        {
+            playTimeText.text = "Thời gian chơi: 00:00";
+        }
+    }
+    public void SetPhysicalCameraByScene(Scene scene)
+    {
+        if (mainCamera != null)
+        {
+            // Ví dụ: chỉ bật Physical Camera ở cảnh "Level_1" và "Level_2"
+            if (scene.name == "1-Boss" || scene.name == "2-Boss"||scene.name=="3-Boss")
+            {
+                mainCamera.usePhysicalProperties = false;
+            }
+            else
+            {
+                mainCamera.usePhysicalProperties = true;
+            }
+        }
+    }
     private void OnDestroy()
     {
-        // Gỡ đăng ký nếu object bị huỷ
         if (instance == this)
             SceneManager.sceneLoaded -= OnSceneLoaded;
     }
