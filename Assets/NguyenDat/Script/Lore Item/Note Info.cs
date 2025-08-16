5f8b1768a220a32902ae99314e0bedaa4e7dd645
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-08-16 23:34:25 +0700

    .

--- Assets/NguyenDat/Script/Lore Item/Note Info.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class NoteInfo : MonoBehaviour
-{
-    public string noteTitle; // Tiêu đề của ghi chú
-    public Sprite noteIcon; // Biểu tượng của ghi chú
-    public string noteDescription; // Mô tả chi tiết của ghi chú
-}
