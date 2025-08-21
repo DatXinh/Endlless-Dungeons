91ed0e03355b2f6ea95cd92de7fc214c9b7ad86d
QuanDZ <164894096+kathy6969@users.noreply.github.com>
2025-08-21 08:47:07 +0700

    update

---
+++ Assets/Quan/Scripts/Firebase/FirebaseInit.cs
@@ -0,0 +1,21 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            if (task.Result == DependencyStatus.Available)
+            {
+                Debug.Log("✅ Firebase sẵn sàng!");
+            }
+            else
+            {
+                Debug.LogError("❌ Firebase lỗi: " + task.Result);
+            }
+        });
+    }
+}
